45a9de9f8eea79a06b6a9f0b898f8c12bc5d4929
Paula Leite <paula.t.leite@icloud.com>
2020-06-21 20:58:19 -0300

    Capable of adding the file

--- FileSharingP2P/ViewController.swift
+++ FileSharingP2P/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func insertDocument(_ sender: Any) {
+        let documentPicker = UIDocumentPickerViewController(documentTypes: [kUTTypePlainText as String], in: .import)
+        documentPicker.delegate = self
+        documentPicker.allowsMultipleSelection = false
+        present(documentPicker, animated: true, completion: nil)
+        
     }
 
+}
 
+extension ViewController: UIDocumentPickerDelegate {
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let selectedFileURL = urls.first else {
+            return
+        }
+        
+        let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let sandboxFileURL = dir.appendingPathComponent(selectedFileURL.lastPathComponent)
+        
+        if FileManager.default.fileExists(atPath: sandboxFileURL.path) {
+            print("File already exists. No need to do anything.")
+        } else {
+            do {
+                try FileManager.default.copyItem(at: selectedFileURL, to: sandboxFileURL)
+                
+                print("Copied file")
+            } catch {
+                print("Error: \(error)")
+            }
+        }
+        
+        
+    }
 }
 
