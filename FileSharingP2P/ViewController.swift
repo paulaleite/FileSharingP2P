d8ea70300367b1f326613bd9d8d62fad090742ba
Paula Leite <paula.t.leite@icloud.com>
2020-06-21 22:06:07 -0300

    File being added

--- FileSharingP2P/ViewController.swift
+++ FileSharingP2P/ViewController.swift
@@ -8,11 +8,45 @@
 
 import UIKit
 import MobileCoreServices
+import MultipeerConnectivity
 
 class ViewController: UIViewController {
+    
+    var files = [URL]()
+    
+    // How someone's name is shown in other devices
+    var peerID = MCPeerID(displayName: UIDevice.current.name)
+    var mcSession: MCSession?
+    var mcAdvertiserAssistant: MCAdvertiserAssistant?
 
     override func viewDidLoad() {
+        title = "File Sharing P2P"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
         
+        // How someone's name is shown in other devices
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        mcSession?.delegate = self
+    }
+    
+    func startHosting(action: UIAlertAction) {
+        guard let mcSession = mcSession else { return }
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "ptl-fileShare", discoveryInfo: nil, session: mcSession)
+        mcAdvertiserAssistant?.start()
+    }
+    
+    func joinSession(action: UIAlertAction) {
+        guard let mcSession = mcSession else { return }
+        let mcBrowser = MCBrowserViewController(serviceType: "ptl-fileShare", session: mcSession)
+        mcBrowser.delegate = self
+        present(mcBrowser, animated: true)
+    }
+    
+    @objc func showConnectionPrompt() {
+        let ac = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Host a Session", style: .default, handler: startHosting))
+        ac.addAction(UIAlertAction(title: "Join a Session", style: .default, handler: joinSession))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
     }
     
     @IBAction func insertDocument(_ sender: Any) {
@@ -23,6 +57,49 @@ class ViewController: UIViewController {
         
     }
 
+}
+
+extension ViewController: MCBrowserViewControllerDelegate, MCSessionDelegate {
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case .connected:
+            print("Connected: \(peerID.displayName)")
+            
+        case .connecting:
+            print("Connectting: \(peerID.displayName)")
+            
+        case .notConnected:
+            print("Not Connected: \(peerID.displayName)")
+            
+        @unknown default:
+            print("Unknown state received: \(peerID.displayName)")
+        }
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        // Not sure what should be implemented here...
+        print("Did receive data")
+    }
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }
 }
 
 extension ViewController: UIDocumentPickerDelegate {
@@ -40,9 +117,24 @@ extension ViewController: UIDocumentPickerDelegate {
             do {
                 try FileManager.default.copyItem(at: selectedFileURL, to: sandboxFileURL)
                 
+                files.append(sandboxFileURL)
+                
                 print("Copied file")
             } catch {
                 print("Error: \(error)")
+            }
+        }
+        
+        guard let mcSession = mcSession else { return }
+        // Sending data to connected peers
+        if mcSession.connectedPeers.count > 0 {
+            let fileData = sandboxFileURL.dataRepresentation
+            do {
+                try mcSession.send(fileData, toPeers: mcSession.connectedPeers, with: .reliable)
+            } catch {
+                let ac = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                present(ac, animated: true)
             }
         }
         
